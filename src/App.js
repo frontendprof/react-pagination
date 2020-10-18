366cf461cf306ca793748df44872da2fdad2c7cb
frontendprof <abdumaliksharipov@gmail.com>
2020-10-18 21:58:34 +0500

    Logic of state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
 
+
+
 function App() {
+
+  const [posts,setPosts]=useState([]);
+  const [loading,setLoading]=useState(false);
+  const [currentPage,setCurrentPage]=useState(1);
+  const [postsPerPage,setPostsPerPage]=useState(10);
+
+
+  useEffect(()=>{
+    const fetchPosts=async()=>{
+      setLoading(true);
+      const res=await axios.get("https://jsonplaceholder.typicode.com/posts");
+      
+      setPosts(res.data)
+      setLoading(false)
+    }
+    fetchPosts();
+  },[])
+  console.log(posts);
+
   return (
     <div className="App">
-     
+      <h1>REact APP</h1>
     </div>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Post = ({posts,loading}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default Post;
